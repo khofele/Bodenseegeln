1f3fc13613d6fc189806c8ed08e4b7f5bcb435ee
khofele <karen.hofele@gmx.de>
2026-03-21 13:02:59 +0100

    [added] manager class

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public static Manager Instance { get; private set; }
+
+    public void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}

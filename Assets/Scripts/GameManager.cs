ef31a5a2bd5a02dd4d6262648fea4179d91042b0
Natalie Prestel <nprestel@stickystonestudio.de>
2026-03-26 00:38:39 +0100

    [changed] GameManager, Manager and UIManager for better singleton handling

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
 
-public class GameManager : Manager
+public class GameManager : Manager<GameManager>
 {
     private GameStates m_currentGameState = GameStates.PAUSED; // TODO Block inputs on start
     // TODO maybe method for setting current game state outside of game manager
     
     [SerializeField] private UIManager m_uiManager = null;
+
+    public void SetState(GameStates _newState)
+    {
+        m_currentGameState = _newState;
+
+        Debug.Log($"Game State changed to: {_newState}");
+    }
 
     public void Start()
     {

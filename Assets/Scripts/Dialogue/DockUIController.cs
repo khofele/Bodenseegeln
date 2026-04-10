fa12e705bce197c0fd58f498f172f4cc9310c925
Natalie Prestel <nprestel@stickystonestudio.de>
2026-04-11 01:01:06 +0200

    [added] Hafeninteraktion und entsprechende Tastenbelegung und kontextspeziefische Inputblockade

---
+++ Assets/Scripts/Dialogue/DockUIController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Dialogue;
+
+public class DockUIController : MonoBehaviour
+{
+    [Header("Buttons")]
+    [SerializeField] private Button m_talkButton = null;
+    [SerializeField] private Button m_repairButton = null;
+    [SerializeField] private Button m_fuelButton = null;
+    [SerializeField] private Button m_closeButton = null;
+
+    private DockInteraction m_currentDock = null;
+
+    public void Init(DockInteraction _dock)
+    {
+        m_currentDock = _dock;
+
+        //clear old listeners
+        m_talkButton.onClick.RemoveAllListeners();
+        m_repairButton.onClick.RemoveAllListeners();
+        m_fuelButton.onClick.RemoveAllListeners();
+        m_closeButton.onClick.RemoveAllListeners();
+
+        //add listeners
+        m_talkButton.onClick.AddListener(OnTalkClicked);
+        m_repairButton.onClick.AddListener(OnRepairClicked);
+        m_fuelButton.onClick.AddListener(OnFuelClicked);
+        m_closeButton.onClick.AddListener(OnCloseClicked);
+    }
+
+    private void OnTalkClicked()
+    {
+        m_currentDock.StartDialogue();
+    }
+
+    private void OnRepairClicked()
+    {
+        Debug.Log("[DockUIController.OnRepairClicked] -not implemented yet");
+    }
+
+    private void OnFuelClicked()
+    {
+        Debug.Log("[DockUIController.OnFuelClicked] -not implemented yet");
+    }
+
+    private void OnCloseClicked()
+    {
+        m_currentDock.CloseDockUI();
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class GameManager : Manager<GameManager>
 {
-    private GameStates m_currentGameState = GameStates.PAUSED; // TODO Block inputs on start
+    private GameStates m_currentGameState = GameStates.PAUSED; // TODO Block inputs on start -> handle this in InputManager
     // TODO maybe method for setting current game state outside of game manager
     
     [SerializeField] private UIManager m_uiManager = null;
+
+    public GameStates CurrentState => m_currentGameState;
 
     public void SetState(GameStates _newState)
     {

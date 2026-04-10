95f221ab5ab9b13735f5011a7ce4f6e6fcb7bdcc
khofele <karen.hofele@gmx.de>
2026-04-11 00:12:39 +0200

    [edited] game manager: added property for current game state + changed default game state for testing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class GameManager : Manager
 {
-    private GameStates m_currentGameState = GameStates.PAUSED; // TODO Block inputs on start
+    // TODO Default-Modus wieder auf PAUSED setzen --> nur für Testzwecke
+    private GameStates m_currentGameState = GameStates.MOTORMODE; // TODO Block inputs on start
     // TODO maybe method for setting current game state outside of game manager
     
     [SerializeField] private UIManager m_uiManager = null;
+
+    public GameStates CurrentGameState
+    {
+        get
+        {
+            return m_currentGameState;
+        }
+    }
 
     public void Start()
     {

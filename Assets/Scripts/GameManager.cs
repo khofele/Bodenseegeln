1b1cfc8cade8b3f2bf76ed95d49c9467fa1cdbd6
khofele <karen.hofele@gmx.de>
2026-04-16 01:16:29 +0200

    [edited] game state to motormode for testing reasons

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class GameManager : Manager
 {
     // TODO Default-Modus wieder auf PAUSED setzen --> nur für Testzwecke
-    private GameStates m_currentGameState = GameStates.SAILMODE; // TODO Block inputs on start
+    private GameStates m_currentGameState = GameStates.MOTORMODE; // TODO Block inputs on start
     // TODO maybe method for setting current game state outside of game manager
     
     [SerializeField] private UIManager m_uiManager = null;

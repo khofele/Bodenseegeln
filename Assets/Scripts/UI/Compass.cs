73e2ed56beb9611c34991ddaf92f950adcda6e4f
Natalie Prestel <nprestel@stickystonestudio.de>
2026-05-17 19:49:03 +0200

    [edited] compass for MainInagameUI

--- Assets/Scripts/UI/Compass.cs
+++ Assets/Scripts/UI/Compass.cs
@@ -1,43 +1,94 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using Dialogue;
+using Quest;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Compass : MonoBehaviour
 {
+    public static Compass Instance { get; private set; }
+
+    [Header("UI")]
     [SerializeField] private RawImage m_compassImage;
     [SerializeField] private Transform m_boatTransform;
-    /*[SerializeField]*/ private float m_maxDiatance = 2000f; //doesnt need this, but better to have it for scaling icons -> so just set it to cover entire map
+    [SerializeField] private GameObject m_iconPrefab;
 
-    [SerializeField] private GameObject m_IconPrefab;
+    [Header("Default Icons")]
+    [SerializeField] private Sprite m_defaultIcon;
+    [SerializeField] private Sprite m_questIcon;
+    [SerializeField] private Sprite m_talkIcon;
 
-    private List<QuestMarker> m_questMarkers = new List<QuestMarker>();
+    private float m_maxDiatance = 2000f; //doesnt need this, but better to have it for scaling icons -> so just set it to cover entire map
+
+    private List<CompassTargetBase> m_targets = new List<CompassTargetBase>();
 
     private float m_compassUnit;
 
-    //just temporary references -> TODO: change them to REAL REFERENCES IN GAME
-    //public QuestMarker one;
+    public Sprite DefaultIcon => m_defaultIcon;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     private void Start()
     {
         m_compassUnit = m_compassImage.rectTransform.rect.width / 360f;
-
-        //also only for testing -> TODO: HANDLE WITH REAL INGAME STUFF
-        //AddQuestMarker(one);
     }
 
     private void Update()
     {
-        m_compassImage.uvRect = new Rect(m_boatTransform.localEulerAngles.y / 360f, 0f, 1f, 1f);
-
-        for (int i = 0; i < m_questMarkers.Count; i++)
+        if (m_boatTransform == null)
         {
-            m_questMarkers[i].m_image.rectTransform.anchoredPosition = GetPosOnCompass(m_questMarkers[i]);
+            return;
+        }
 
-            float _distance = Vector2.Distance(new Vector2(m_boatTransform.transform.position.x, m_boatTransform.transform.position.z), m_questMarkers[i].Position);
+        UpdateCompassRotation();
+        UpdateTargets();
+    }
 
-            Debug.Log($"[Compass] distance = {_distance}, of max distance = {m_maxDiatance}, so scle = 1 - ({(_distance / m_maxDiatance)})");
-            
+    private void UpdateCompassRotation()
+    {
+        m_compassImage.uvRect = new Rect(m_boatTransform.localEulerAngles.y / 360f, 0f, 1f, 1f);
+    }
+
+    private void UpdateTargets()
+    {
+        for (int i = 0; i < m_targets.Count; i++)
+        {
+            CompassTargetBase _target = m_targets[i];
+
+            if (_target == null)
+            {
+                continue;
+            }
+
+            if (_target.Image == null)
+            {
+                CreateUI(_target);
+            }
+
+            if (_target.Image == null)
+            {
+                continue;
+            }
+
+            bool _shouldShow = _target.ShouldShowIcon();
+
+            _target.SetVisible(_shouldShow);
+
+            if (!_shouldShow)
+            {
+                continue;
+            }
+
+            UpdateTargetIcon(_target);
+
+            _target.Image.rectTransform.anchoredPosition = GetPos(_target);
+
+            //scale based distance
+            Vector2 _boatPos = new Vector2(m_boatTransform.position.x, m_boatTransform.position.z);
+            float _distance = Vector2.Distance(_boatPos, _target.GetPosition());
             float _scale = 0f;
 
             if (_distance < m_maxDiatance)
@@ -45,26 +96,91 @@ public class Compass : MonoBehaviour
                 _scale = 1f - (_distance / m_maxDiatance);
             }
 
-            m_questMarkers[i].m_image.rectTransform.localScale = Vector3.one * _scale;
+            _target.Image.rectTransform.localScale = Vector3.one * _scale;
         }
     }
 
-    public void AddQuestMarker (QuestMarker _marker)
+    private void UpdateTargetIcon(CompassTargetBase _target)
     {
-        GameObject _newMarker = Instantiate(m_IconPrefab, m_compassImage.transform);
-        _marker.m_image = _newMarker.GetComponent<Image>();
-        _marker.m_image.sprite = _marker.Icon;
+        QuestInteraction _quest = _target as QuestInteraction;
 
-        m_questMarkers.Add(_marker);
+        if (_quest != null)
+        {
+            _target.Image.sprite = m_questIcon;
+            return;
+        }
+
+        NPCInteraction _npc = _target as NPCInteraction;
+
+        if (_npc != null)
+        {
+            NPCIconState _state = _npc.GetCompassIconState();
+
+            switch (_state)
+            {
+                case NPCIconState.Quest:
+                    _target.Image.sprite = m_questIcon;
+                    return;
+
+                case NPCIconState.Talk:
+                    _target.Image.sprite = m_talkIcon;
+                    return;
+
+                case NPCIconState.None:
+                default:
+                    return;
+            }
+        }
+
+        _target.Image.sprite = _target.GetIcon();
     }
 
-    private Vector2 GetPosOnCompass (QuestMarker _marker)
+    private void CreateUI(CompassTargetBase _target)
     {
-        Vector2 _boatPos = new Vector2(m_boatTransform.transform.position.x, m_boatTransform.transform.position.z);
-        Vector2 _boatFwd = new Vector2(m_boatTransform.transform.forward.x, m_boatTransform.transform.forward.z);
+        GameObject _go = Instantiate(m_iconPrefab, m_compassImage.transform);
+        Image _img = _go.GetComponent<Image>();
 
-        float _angle = Vector2.SignedAngle(_marker.Position - _boatPos, _boatFwd);
+        _target.BindImage(_img);
+    }
+
+    private Vector2 GetPos(CompassTargetBase _target)
+    {
+        Vector2 _boatPos = new Vector2(m_boatTransform.position.x, m_boatTransform.position.z);
+        Vector2 _targetPos = _target.GetPosition();
+        Vector2 _direction =_targetPos - _boatPos;
+        Vector2 _boatForward = new Vector2(m_boatTransform.forward.x, m_boatTransform.forward.z);
+
+        float _angle = Vector2.SignedAngle(_direction, _boatForward);
 
         return new Vector2(m_compassUnit * _angle, 0f);
     }
+
+    public void Register(CompassTargetBase _target)
+    {
+        if (_target == null)
+        {
+            return;
+        }
+
+        if (!m_targets.Contains(_target))
+        {
+            m_targets.Add(_target);
+        }
+    }
+
+    public void Unregister(CompassTargetBase _target)
+    {
+        if (_target == null)
+        {
+            return;
+        }
+
+        if (_target.Image != null)
+        {
+            Destroy(_target.Image.gameObject);
+        }
+
+        m_targets.Remove(_target);
+    }
 }
+

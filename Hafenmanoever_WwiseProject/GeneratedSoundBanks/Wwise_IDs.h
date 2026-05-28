e2a62d90a4163aa0efa44e7f6a5c21d4db28db2e
thisishenni <thisishenni@gmail.com>
2026-05-28 18:06:42 +0200

    [fixed] broken engine audio files and playback limit

--- Hafenmanoever_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
+++ Hafenmanoever_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,14 +13,35 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID AMB_START_COAST = 2069355313U;
+        static const AkUniqueID AMB_START_GARAGE = 2203898210U;
+        static const AkUniqueID AMB_START_HARBORBREGENZ = 1802511244U;
+        static const AkUniqueID AMB_START_HARBORHARD = 1772974160U;
+        static const AkUniqueID AMB_START_HARBORLINDAU = 2966838670U;
+        static const AkUniqueID AMB_START_HARBORLOCHAU = 222490955U;
+        static const AkUniqueID AMB_START_SEAGULLS = 3868672069U;
+        static const AkUniqueID AMB_START_WATERMOVEMENT = 1406048945U;
+        static const AkUniqueID AMB_STOP_COAST = 3608418441U;
+        static const AkUniqueID AMB_STOP_GARAGE = 691410714U;
+        static const AkUniqueID AMB_STOP_HARBORBREGENZ = 237785684U;
+        static const AkUniqueID AMB_STOP_HARBORHARD = 3691877560U;
+        static const AkUniqueID AMB_STOP_HARBORLINDAU = 1539578486U;
+        static const AkUniqueID AMB_STOP_HARBORLOCHAU = 2951909939U;
+        static const AkUniqueID AMB_STOP_SEAGULLS = 2209190525U;
+        static const AkUniqueID AMB_STOP_WATERMOVEMENT = 451382329U;
         static const AkUniqueID BOAT_ENGINESTART = 1385959304U;
         static const AkUniqueID BOAT_ENGINESTARTKEY = 480691325U;
         static const AkUniqueID BOAT_ENGINESTOP = 1816964564U;
         static const AkUniqueID BOAT_ENGINEWATERSTART = 765304779U;
         static const AkUniqueID BOAT_ENGINEWATERSTOP = 1447191913U;
+        static const AkUniqueID BOAT_RIGGINGMOVE = 2909675614U;
+        static const AkUniqueID BOAT_RIGGINGTENSION = 4170174229U;
+        static const AkUniqueID BOAT_ROPECREAK = 4277963706U;
         static const AkUniqueID BOAT_SAILDEPLOY = 52134156U;
         static const AkUniqueID BOAT_SAILRETRACT = 509806472U;
+        static const AkUniqueID BOAT_START_CREAKING = 3654424549U;
         static const AkUniqueID BOAT_START_SAILFLAP = 3762695479U;
+        static const AkUniqueID BOAT_STOP_CREAKING = 2290206829U;
         static const AkUniqueID BOAT_STOP_SAILFLAP = 2177072159U;
         static const AkUniqueID BOAT_THROTTLEMOVE = 3488054365U;
         static const AkUniqueID BOAT_THROTTLEMOVEIDLE = 1287245143U;
@@ -29,8 +50,16 @@ namespace AK
         static const AkUniqueID BOAT_WOODVIBRATIONSTOP = 551126337U;
         static const AkUniqueID ENV_BOATCOLLISIONHEAVY = 637864694U;
         static const AkUniqueID ENV_BOATCOLLISIONLIGHT = 4135813027U;
+        static const AkUniqueID ENV_BOATCOLLISIONUNDERWATER = 3096912414U;
+        static const AkUniqueID ENV_BOATDOCKIMPACT = 3711998144U;
+        static const AkUniqueID ENV_BOATREFUEL = 2984994092U;
+        static const AkUniqueID ENV_BOATREPAIR = 4191093004U;
         static const AkUniqueID ENV_CLOSEMAP = 2562124403U;
         static const AkUniqueID ENV_OPENMAP = 4045900857U;
+        static const AkUniqueID ENV_START_BOAT_RUBDOCK = 1473971111U;
+        static const AkUniqueID ENV_START_WATERDOCK = 2930601010U;
+        static const AkUniqueID ENV_STOP_BOAT_RUBDOCK = 1068606741U;
+        static const AkUniqueID ENV_STOP_WATERDOCK = 3399555120U;
         static const AkUniqueID MENU_AMBIENTSTART = 4290233515U;
         static const AkUniqueID MENU_AMBIENTSTOP = 3178454985U;
         static const AkUniqueID MENU_NAVIGATEBACKWARD = 2498328905U;
@@ -52,6 +81,23 @@ namespace AK
         static const AkUniqueID MISSION_MONEYZERO = 3701861410U;
         static const AkUniqueID MISSION_START_VOICELINESNPC = 3522430105U;
         static const AkUniqueID MISSION_STOP_VOICELINESNPC = 2630645085U;
+        static const AkUniqueID PHY_START_BOWWATER = 2610887575U;
+        static const AkUniqueID PHY_START_SAILMASTWHISTLE = 1227687800U;
+        static const AkUniqueID PHY_START_WAKEWATER = 1373022075U;
+        static const AkUniqueID PHY_START_WATERDISPLACEMENT = 2223292318U;
+        static const AkUniqueID PHY_START_WAVEHULL = 3237319048U;
+        static const AkUniqueID PHY_START_WAVEMOTION = 333473287U;
+        static const AkUniqueID PHY_START_WINDFORCEBOAT = 2742153359U;
+        static const AkUniqueID PHY_START_WINDFORCESAIL = 1834823086U;
+        static const AkUniqueID PHY_STOP_BOWWATER = 1257340305U;
+        static const AkUniqueID PHY_STOP_SAILMASTWHISTLE = 421419538U;
+        static const AkUniqueID PHY_STOP_WAKEWATER = 3538696357U;
+        static const AkUniqueID PHY_STOP_WATERDISPLACEMENT = 494378860U;
+        static const AkUniqueID PHY_STOP_WAVEHULL = 3006133870U;
+        static const AkUniqueID PHY_STOP_WAVEMOTION = 825025977U;
+        static const AkUniqueID PHY_STOP_WINDFORCEBOAT = 3683395289U;
+        static const AkUniqueID PHY_STOP_WINDFORCESAIL = 677883868U;
+        static const AkUniqueID PHY_WINDGUST = 4095079910U;
     } // namespace EVENTS
 
     namespace GAME_PARAMETERS

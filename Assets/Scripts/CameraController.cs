c738aab015c0ff8289be03737704beaf71af6abc
khofele <karen.hofele@gmx.de>
2026-05-31 00:56:05 +0200

    [edited] camera controller: restructured camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,44 +4,77 @@ using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
+    // GENERAL FIELDS
+    [SerializeField] private GameManager m_gameManager = null;
     [SerializeField] private CinemachineOrbitalFollow m_orbitalFollow = null;
+
+    // INPUT ACTIONS
     [SerializeField] private InputActionReference m_resetCameraAction = null;
-    [SerializeField] private InputActionReference m_lookAction = null;
+    [SerializeField] private InputActionReference m_mouseLookAction = null;
+    [SerializeField] private InputActionReference m_lookLeftAction = null;
+    [SerializeField] private InputActionReference m_lookRightAction = null;
 
     private bool m_isResetting = false;
 
     private void OnEnable()
     {
         if (m_resetCameraAction != null)
+        {
             m_resetCameraAction.action.Enable();
+        }
 
-        if (m_lookAction != null)
-            m_lookAction.action.Enable();
+        if (m_mouseLookAction != null)
+        {
+            m_mouseLookAction.action.Enable();
+        }
+
+        if (m_lookLeftAction != null)
+        {
+            m_lookLeftAction.action.Enable();
+        }
+
+        if (m_lookRightAction != null)
+        {
+            m_lookRightAction.action.Enable();
+        }
     }
 
     private void OnDisable()
     {
         if (m_resetCameraAction != null)
+        {
             m_resetCameraAction.action.Disable();
+        }
 
-        if (m_lookAction != null)
-            m_lookAction.action.Disable();
+        if (m_mouseLookAction != null)
+        {
+            m_mouseLookAction.action.Disable();
+        }
+
+        if (m_lookLeftAction != null)
+        {
+            m_lookLeftAction.action.Disable();
+        }
+
+        if (m_lookRightAction != null)
+        {
+            m_lookRightAction.action.Disable();
+        }
     }
 
-    private void Update()
+    private void CheckStartReset()
     {
-        if (m_orbitalFollow == null)
-            return;
-
-        if (m_resetCameraAction != null &&
-            m_resetCameraAction.action.triggered)
+        if (m_resetCameraAction != null && m_resetCameraAction.action.triggered == true)
         {
             StartReset();
         }
+    }
 
-        if (m_isResetting && m_lookAction != null)
+    private void TriggerStopReset()
+    {
+        if (m_isResetting && m_mouseLookAction != null)
         {
-            Vector2 lookInput = m_lookAction.action.ReadValue<Vector2>();
+            Vector2 lookInput = m_mouseLookAction.action.ReadValue<Vector2>();
 
             if (lookInput.sqrMagnitude > 0.0001f)
             {
@@ -70,5 +103,34 @@ public class CameraController : MonoBehaviour
     private void StopReset()
     {
         m_isResetting = false;
+    }
+
+    private void TriggerLookLeftRight()
+    {
+        if (m_lookLeftAction.action.triggered == true)
+        {
+            // TODO implement: lerp to left
+        }
+
+        if (m_lookRightAction.action.triggered == true)
+        {
+            // TODO implement: lerp to right
+        }
+    }
+
+    private void Update()
+    {
+        if(m_gameManager.CurrentState == GameStates.SAILMODE || m_gameManager.CurrentState == GameStates.MOTORMODE)
+        {
+            if (m_orbitalFollow == null)
+            {
+                return;
+            }
+
+            TriggerLookLeftRight();
+
+            CheckStartReset();
+            TriggerStopReset();
+        }
     }
 }

5af81964644dcbe5802551261d5e3b8dc596bd52
Natalie Prestel <nprestel@stickystonestudio.de>
2026-06-02 22:04:29 +0200

    [edited] Mask to compass to only visualize the center of the compass, [added] icon for where the boat is heading to

--- Assets/Scripts/UI/Compass.cs
+++ Assets/Scripts/UI/Compass.cs
@@ -10,6 +10,7 @@ public class Compass : MonoBehaviour
 
     [Header("UI")]
     [SerializeField] private RawImage m_compassImage;
+    [SerializeField] private Transform m_cameraTransform;
     [SerializeField] private Transform m_boatTransform;
     [SerializeField] private GameObject m_iconPrefab;
 
@@ -17,6 +18,9 @@ public class Compass : MonoBehaviour
     [SerializeField] private Sprite m_defaultIcon;
     [SerializeField] private Sprite m_questIcon;
     [SerializeField] private Sprite m_talkIcon;
+
+    [Header("Boat Direction")]
+    [SerializeField] private Image m_boatDirectionIcon;
 
     private float m_maxDiatance = 2000f; //doesnt need this, but better to have it for scaling icons -> so just set it to cover entire map
 
@@ -38,18 +42,19 @@ public class Compass : MonoBehaviour
 
     private void Update()
     {
-        if (m_boatTransform == null)
+        if (m_cameraTransform == null || m_boatTransform == null)
         {
             return;
         }
 
         UpdateCompassRotation();
+        UpdateBoatDirectionIcon();
         UpdateTargets();
     }
 
     private void UpdateCompassRotation()
     {
-        m_compassImage.uvRect = new Rect(m_boatTransform.localEulerAngles.y / 360f, 0f, 1f, 1f);
+        m_compassImage.uvRect = new Rect(m_cameraTransform.localEulerAngles.y / 360f, 0f, 1f, 1f);
     }
 
     private void UpdateTargets()
@@ -87,7 +92,7 @@ public class Compass : MonoBehaviour
             _target.Image.rectTransform.anchoredPosition = GetPos(_target);
 
             //scale based distance
-            Vector2 _boatPos = new Vector2(m_boatTransform.position.x, m_boatTransform.position.z);
+            Vector2 _boatPos = new Vector2(m_cameraTransform.position.x, m_cameraTransform.position.z);
             float _distance = Vector2.Distance(_boatPos, _target.GetPosition());
             float _scale = 0f;
 
@@ -135,6 +140,26 @@ public class Compass : MonoBehaviour
         _target.Image.sprite = _target.GetIcon();
     }
 
+    private void UpdateBoatDirectionIcon()
+    {
+        if (m_boatDirectionIcon == null)
+        {
+            return;
+        }
+
+        if (m_boatTransform == null || m_cameraTransform == null)
+        {
+            return;
+        }
+
+        Vector2 _cameraForward = new Vector2(m_cameraTransform.forward.x, m_cameraTransform.forward.z);
+        Vector2 _boatForward = new Vector2(m_boatTransform.forward.x, m_boatTransform.forward.z);
+
+        float _angle = Vector2.SignedAngle(_boatForward, _cameraForward);
+
+        m_boatDirectionIcon.rectTransform.anchoredPosition = new Vector2(m_compassUnit * _angle, 0f);
+    }
+
     private void CreateUI(CompassTargetBase _target)
     {
         GameObject _go = Instantiate(m_iconPrefab, m_compassImage.transform);
@@ -145,10 +170,10 @@ public class Compass : MonoBehaviour
 
     private Vector2 GetPos(CompassTargetBase _target)
     {
-        Vector2 _boatPos = new Vector2(m_boatTransform.position.x, m_boatTransform.position.z);
+        Vector2 _boatPos = new Vector2(m_cameraTransform.position.x, m_cameraTransform.position.z);
         Vector2 _targetPos = _target.GetPosition();
         Vector2 _direction =_targetPos - _boatPos;
-        Vector2 _boatForward = new Vector2(m_boatTransform.forward.x, m_boatTransform.forward.z);
+        Vector2 _boatForward = new Vector2(m_cameraTransform.forward.x, m_cameraTransform.forward.z);
 
         float _angle = Vector2.SignedAngle(_direction, _boatForward);
 

0682959c9c3e3021771f66c43060a70988d6c5be
khofele <karen.hofele@gmx.de>
2026-06-14 19:38:40 +0200

    [edited] pause menu code: changed button function

--- Assets/Scripts/UI/PauseMenuUI.cs
+++ Assets/Scripts/UI/PauseMenuUI.cs
@@ -2,7 +2,6 @@ using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using WwiseRTPC = AK.Wwise.RTPC;
 
 public class PauseMenuUI : MonoBehaviour
 {
@@ -10,46 +9,27 @@ public class PauseMenuUI : MonoBehaviour
 
     [Header("UI Components")]
     [SerializeField] private Button m_btnResumeGame = null;
-    [SerializeField] private Button m_btnBackToMainMenu = null;
-    [SerializeField] private Slider m_sliderVolume = null;
+    [SerializeField] private Button m_btnEndGame = null;
 
-    [Header("WWise References")]
-    [SerializeField] private WwiseRTPC m_masterVolumeRTPC = null;
-
-    private void BackToMainMenu()
+    private void EndGame()
     {
-        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
-    }
-
-    private void SetVolume(float volume)
-    {
-        float clampedVolume = Mathf.Clamp(volume, 0.0f, 100.0f);
-
-        if (m_masterVolumeRTPC != null)
-        {
-            m_masterVolumeRTPC.SetGlobalValue(clampedVolume);
-        }
-
-        // TODO Lautstärkewert global speichern für alle Menüs
+        Application.Quit();
     }
 
     private void Start()
     {
         m_btnResumeGame.onClick.AddListener(() => OnResumeButtonClicked?.Invoke());
-        m_btnBackToMainMenu.onClick.AddListener(BackToMainMenu);
-        m_sliderVolume.onValueChanged.AddListener((float volume) => SetVolume(volume));
+        m_btnEndGame.onClick.AddListener(EndGame);
     }
 
-    public void OnEnable()
+    private void OnEnable()
     {
-        Debug.Log("Pausem ON");
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.Confined;
     }
 
-    public void OnDisable()
+    private void OnDisable()
     {
-        Debug.Log("Pausem OFF");
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }

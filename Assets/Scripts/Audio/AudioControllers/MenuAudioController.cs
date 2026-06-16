31d60a2c8c77940e03b2178c9f1bfa4887fdfc32
thisishenni <thisishenni@gmail.com>
2026-06-16 16:05:32 +0200

    [edited] audio prefab and audio script folder structure [added] Menu Audio Controller and MissionsFeedback Controller Prefabs with scripts

---
+++ Assets/Scripts/Audio/AudioControllers/MenuAudioController.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using WwiseEvent = AK.Wwise.Event;
+
+public class MenuAudioController : MonoBehaviour
+{
+    [Header("Menu Navigation Events")]
+    [SerializeField] private WwiseEvent m_navigateForwardEvent = null;
+    [SerializeField] private WwiseEvent m_navigateBackwardEvent = null;
+
+    [Header("Menu Open Events")]
+    [SerializeField] private WwiseEvent m_openMainMenuEvent = null;
+    [SerializeField] private WwiseEvent m_openPauseMenuEvent = null;
+
+    [Header("Settings Events")]
+    [SerializeField] private WwiseEvent m_modifiedSettingsEvent = null;
+
+    [Header("Game Start Event")]
+    [SerializeField] private WwiseEvent m_startGameEvent = null;
+
+    [Header("Main Menu Ambient Events")]
+    [SerializeField] private WwiseEvent m_menuAmbientStartEvent = null;
+    [SerializeField] private WwiseEvent m_menuAmbientStopEvent = null;
+
+    // PRIVATE METHODS ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private void PlayEvent(WwiseEvent wwiseEvent)
+    {
+        if (wwiseEvent == null)
+        {
+            return;
+        }
+
+        wwiseEvent.Post(gameObject);
+    }
+
+    // PUBLIC METHODS ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public void PlayNavigateForward()
+    {
+        PlayEvent(m_navigateForwardEvent);
+    }
+
+    public void PlayNavigateBackward()
+    {
+        PlayEvent(m_navigateBackwardEvent);
+    }
+
+    public void PlayOpenMainMenu()
+    {
+        PlayEvent(m_openMainMenuEvent);
+    }
+
+    public void PlayOpenPauseMenu()
+    {
+        PlayEvent(m_openPauseMenuEvent);
+    }
+
+    public void PlayModifiedSettings()
+    {
+        PlayEvent(m_modifiedSettingsEvent);
+    }
+
+    public void PlayStartGame()
+    {
+        PlayEvent(m_startGameEvent);
+    }
+
+    public void StartMenuAmbient()
+    {
+        PlayEvent(m_menuAmbientStartEvent);
+    }
+
+    public void StopMenuAmbient()
+    {
+        PlayEvent(m_menuAmbientStopEvent);
+    }
+}
---
+++ Assets/Scripts/Audio/AudioControllers/MissionsFeedbackAudioController.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using WwiseEvent = AK.Wwise.Event;
+
+public class MissionsFeedbackAudioController : MonoBehaviour
+{
+    [Header("Mission Feedback Events")]
+    [SerializeField] private WwiseEvent m_missionAcceptEvent = null;
+    [SerializeField] private WwiseEvent m_missionCompleteEvent = null;
+    [SerializeField] private WwiseEvent m_missionFailEvent = null;
+    [SerializeField] private WwiseEvent m_missionAbortEvent = null;
+
+    [Header("Money Events")]
+    [SerializeField] private WwiseEvent m_moneyZeroEvent = null;
+
+    [Header("Goal Events")]
+    [SerializeField] private WwiseEvent m_goalReachedEvent = null;
+    [SerializeField] private WwiseEvent m_goalFailEvent = null;
+
+    [Header("Warning Events")]
+    [SerializeField] private WwiseEvent m_boatDamageEvent = null;
+    [SerializeField] private WwiseEvent m_fastMovementEvent = null;
+
+    // PRIVATE METHODS ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private void PlayEvent(WwiseEvent wwiseEvent)
+    {
+        if (wwiseEvent == null)
+        {
+            return;
+        }
+
+        wwiseEvent.Post(gameObject);
+    }
+
+    // PUBLIC METHODS ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public void PlayMissionAccept()
+    {
+        PlayEvent(m_missionAcceptEvent);
+    }
+
+    public void PlayMissionComplete()
+    {
+        PlayEvent(m_missionCompleteEvent);
+    }
+
+    public void PlayMissionFail()
+    {
+        PlayEvent(m_missionFailEvent);
+    }
+
+    public void PlayMissionAbort()
+    {
+        PlayEvent(m_missionAbortEvent);
+    }
+
+    public void PlayMoneyZero()
+    {
+        PlayEvent(m_moneyZeroEvent);
+    }
+
+    public void PlayGoalReached()
+    {
+        PlayEvent(m_goalReachedEvent);
+    }
+
+    public void PlayGoalFail()
+    {
+        PlayEvent(m_goalFailEvent);
+    }
+
+    public void PlayBoatDamage()
+    {
+        PlayEvent(m_boatDamageEvent);
+    }
+
+    public void PlayFastMovement()
+    {
+        PlayEvent(m_fastMovementEvent);
+    }
+}

85e19a5ef37cadfc3602a0c325ccd26fa78ddac8
khofele <karen.hofele@gmx.de>
2026-06-16 21:07:30 +0200

    [edited] reset request ui: added functionality

--- Assets/Scripts/UI/ResetRequestUI.cs
+++ Assets/Scripts/UI/ResetRequestUI.cs
@@ -1,6 +1,34 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ResetRequestUI : MonoBehaviour
 {
-    // TODO Screen soll verschwinden nachdem Resetet wurde
+    [Header("UI Components")]
+    [SerializeField] private TextMeshProUGUI m_text = null;
+
+    [Header("Input Action")]
+    [SerializeField] private InputActionReference m_resetBoatAction = null;
+
+    public void ShowScreen()
+    {
+        gameObject.SetActive(true);
+        m_text.text = "Du musst abgeschleppt werden. Drücke R, um dein Boot abschleppen zu lassen!";
+    }
+
+    private void GetResetInput()
+    {
+        if(m_resetBoatAction.action.triggered == true)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if(gameObject.activeSelf == true)
+        {
+            GetResetInput();
+        }
+    }
 }

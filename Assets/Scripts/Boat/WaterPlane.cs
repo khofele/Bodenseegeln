c99f803d68771f6c7a44971649fcfb5176160aa1
khofele <karen.hofele@gmx.de>
2026-07-04 02:49:18 +0200

    [fixed] water plane script

--- Assets/Scripts/Boat/WaterPlane.cs
+++ Assets/Scripts/Boat/WaterPlane.cs
@@ -4,7 +4,6 @@ public class WaterPlane : MonoBehaviour
 {
     public void Update()
     {
-        Vector3 currentEulerAngles = transform.eulerAngles;
-        transform.eulerAngles = new Vector3(currentEulerAngles.x, currentEulerAngles.y, 0.0f);
+        transform.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
     }
 }
